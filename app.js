4300c9b87064fd0af7d84153f097bc5ccb62c1b0
subin <83167647+subin4693@users.noreply.github.com>
2024-08-07 21:48:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.json({ limit: "50mb" }));
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://main--extraordinary-fox-28d76d.netlify.app",
+  "https://lucent-baklava-0dc47d.netlify.app",
 ];
 
 app.use(
